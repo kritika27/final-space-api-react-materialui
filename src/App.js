23698b987977c927fa7ae9f111bd6755ef3fdaf5
kritika <kritikasri27@gmail.com>
2021-04-17 02:29:17 +0530

    Added logic to fetch final space characters from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Form from "./Form";
 
-function App() {
+const url = "https://finalspaceapi.com/api/v0/character/?limit=12&sort=desc";
+
+export default function App() {
+  const useStyles = makeStyles({
+    card: {
+      maxWidth: 345,
+      boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+      backgroundColor: "#fafafa",
+    },
+    media: {
+      height: 300,
+    },
+  });
+  const classes = useStyles();
+  const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const debounced = useDebouncedCallback((search) => {
+    setSearch(search);
+  }, 1000);
+
+  const filteredChar = characters.filter((char) => {
+    return char.name.toLowerCase().includes(search.toLowerCase());
+  });
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCharacters(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Container>
+        <Typography
+          color="textPrimary"
+          gutterBottom
+          variant="h3"
+          align="center"
         >
-          Learn React
-        </a>
-      </header>
+          FINAL SPACE
+        </Typography>
+        <Form
+          onSearchChange={(e) => debounced(e.target.value)}
+          search={search}
+        />
+        <Grid container spacing={3} align="center">
+          {filteredChar.map((char) => (
+            <Grid item xs={12} lg={4} key={char.id}>
+              <Card className={classes.card}>
+                <CardMedia className={classes.media} image={char.img_url} />
+                <CardContent>
+                  <Typography color="primary" variant="h5">
+                    {char.name}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 }
-
-export default App;
